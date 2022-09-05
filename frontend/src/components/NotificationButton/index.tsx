518a6592e749bc9b85846c7fa3a28fa2705714e9
William Ferreira <williamfs133@gmail.com>
2022-09-05 15:56:31 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,5 +1,5 @@
-import icon from '../../assets/img/notification-icon.svg'
-import './styles.css'
+import icon from '../../assets/img/notification-icon.svg';
+import './styles.css';
 
 function NoatificationButton() {
     return (
@@ -10,4 +10,4 @@ function NoatificationButton() {
     )
 }
 
-export default NoatificationButton
+export default NoatificationButton;
