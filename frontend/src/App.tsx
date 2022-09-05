3e15d66e2d765dadb6030752bfde9a79dbc7dfaf
William Ferreira <williamfs133@gmail.com>
2022-09-05 15:17:09 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
+import Header from "./components/Header"
 import NoatificationButton from "./components/NotificationButton"
+import SalesCard from "./components/SalesCard"
 
 function App() {
-  return(
+  return (
     <>
-      <h1>Hello...</h1>
-      <NoatificationButton/>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+          <SalesCard/>
+          </div>
+        </section>
+      </main>
     </>
   )
 }
